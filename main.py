65b14aa976729bd9bb679ed530ba3295820754e6
Rakugen <simonlee0306@gmail.com>
2023-02-17 15:59:58 -0500

    Add player 2 moves, win checks, duplicate move checks, sleep delay, quit option.

--- main.py
+++ main.py
@@ -5,8 +5,10 @@
 # TODO: Create a board out of ASCII characters, 3 rows with 3 columns each, which will be stored in a nested list:
 #  [[row1], [row2], [row3]]
 
-# board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
 import random
+import time
+
+game_on = True
 
 def display_board(board):
     for row in board:
@@ -42,42 +44,53 @@ def check_win(board):
         return False
 
 def player_2_move(board):
+    possible_moves = []
     for x in [0, 1, 2]:
         for y in [0, 1, 2]:
             if board[x][y] == '-':
-                if random.choice([0, 1, 2, 3]) > 1:
-                    return True
+                possible_moves.append((x, y))
+    player2_move = random.choice(possible_moves)
+    return player2_move
 
-while True:
+while game_on:
     ttt_board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
     print("Welcome to Tic-Tac-Toe! You are Player 1.")
-    while True:
+    while game_on:
         display_board(ttt_board)
         # Player 1 move
         while True:
-            player_move = input('What is your move? row# + col#: 00, 01, 21, etc.:')
+            player_move = input('What is your move? row# + col#: 00, 01, 21, etc. or "quit" to stop playing:')
+            if player_move == "quit":
+                game_on = False
+                print("Thank you for playing.")
+                break
             if player_move not in ['00', '01', '02', '10', '11', '12', '20', '21', '22']:
-                print("Invalid input.")
+                print("Invalid input. Enter 2 valid numbers.")
             else:
                 row = int(player_move[0])
                 col = int(player_move[1])
                 if ttt_board[row][col] == '-':
                     ttt_board[row][col] = 'x'
                     break
+                else:
+                    print("Invalid input. That spot is taken.")
+
         # Check win condition
-        print(check_win(ttt_board))
         if check_win(ttt_board):
             print("Player 1 Wins!")
             display_board(ttt_board)
             print('============================================\n')
+            time.sleep(2)
             break
 
         # Player 2 move
-        player_2_move(ttt_board)
+        p2_move = player_2_move(ttt_board)
+        ttt_board[p2_move[0]][p2_move[1]] = 'o'
+        if check_win(ttt_board):
+            print("Player 2 Wins! Try again.")
+            display_board(ttt_board)
+            print('============================================\n')
+            time.sleep(2)
+            break
 
-        # Check board spot
 
-        # Check win condition
-        # if check_win(ttt_board):
-        #     print("Player 2 Wins!")
-        #     break
